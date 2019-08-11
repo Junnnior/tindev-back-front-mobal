a36f09100154b3ebc2b05b2a80ad73b564b0a867
Junnnior <juniorfoxld@hotmail.com>
2019-08-11 18:21:10 -0300

    mostrando lista de usuarios

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import './Login.css';
 import logo from '../assets/logo.svg';
 
-export default function Login() {
+import api from '../services/api';
+
+
+export default function Login({ history }) {
+
+	const [username, setUsername] = useState('');
+
+	async function handleSubmit(e){
+		// evita atualização do submit
+		e.preventDefault();
+
+		const response = await api.post('/devs', {
+			username,
+		});
+
+		// get id username
+		const { _id } = response.data;
+
+		console.log(_id);
+
+		history.push(`/dev/${_id}`);
+	}
+
     return (
         <div className = "login-container">
-        	<form>
+        	<form onSubmit = {handleSubmit}>
         		<img src = { logo } alt = "Tindev"/>
-        		<input placeholder="Usuario Github" name=""/>
+        		<input 
+        			placeholder="Usuario Github" name=""
+        			value = {username}
+        			onChange = {e => setUsername(e.target.value)}
+        		/>
         		<button type="submit">Enviar</button>
         	</form>
         </div>
