3c2cff92fb94e422abee460def5779a15f4f02dd
DavidLuna0 <ddavidr569@gmail.com>
2019-10-02 09:12:27 -0300

    Cleaning code

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -2,11 +2,9 @@ const axios = require('axios');
 const Dev = require('../models/Dev');
 
 module.exports = {
-
     async index(req, res) {
         const { user } = req.headers;
         const loggedDev = await Dev.findById(user);
-
         const users = await Dev.find({
             $and: [
                 // ne = diferente | nin = nao pertence
@@ -15,24 +13,18 @@ module.exports = {
                 { _id: { $nin: loggedDev.dislikes } },
             ],
         })
-
         return res.json(users);
     },
 
     async store(req, res) {
         const { username } = req.body;
-
         // verifica se o usuario existe
         const userExists = await Dev.findOne({ user: username });
-
         if (userExists) {
             return res.json(userExists);
         }
-
         const response = await axios.get(`https://api.github.com/users/${username}`);
-
         const { name, bio, avatar_url: avatar } = response.data;
-
         // cria dev no banco de dados
         const dev = await Dev.create({
             name,
@@ -40,7 +32,6 @@ module.exports = {
             bio,
             avatar
         });
-
         return res.json(dev);
     }
 };
--- backend/src/models/Dev.js
+++ backend/src/models/Dev.js
@@ -5,20 +5,16 @@ const DevSchema = new Schema({
         type: String,
         required: true,
     },
-
     user: {
         type: String,
         required: true,
 
     },
-
     bio: String,
-
     avatar: {
         type: String,
         required: true,
     },
-
     likes: [{
         type: Schema.Types.ObjectId,
         ref: 'Dev',
